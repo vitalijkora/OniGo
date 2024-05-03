aef28e1762930c1704161e2e08d0a4ba5ad9cb2a
Vitalii Kora <Vitalii_Kora@epam.com>
2024-05-03 20:41:38 +0300

    [Feat]: add product tile ui

--- app/src/main/java/com/example/onigo/ui/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/onigo/ui/adapters/CategoryAdapter.kt
@@ -10,6 +10,7 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.product_tile.view.*
 import com.example.onigo.R
 import com.example.onigo.models.COSTCO
+import java.util.Locale
 
 class CategoryAdapter: RecyclerView.Adapter<CategoryAdapter.NewsViewHolder>() {
 
@@ -39,8 +40,9 @@ class CategoryAdapter: RecyclerView.Adapter<CategoryAdapter.NewsViewHolder>() {
             Glide.with(this).load(product_tile.image).into(product_tile_image)
             product_tile_image.clipToOutline = true
             product_tile_title.text = product_tile.title
-            product_tile_price.text = product_tile.price.toString()
-
+            product_tile_price.text = String.format(Locale.JAPAN, "%d", product_tile.price)
+            product_tile_price_with_tax.text=String.format(Locale.JAPAN, "%,d", Math.round(product_tile.priceWithTax))
+            product_tile_unit.text=product_tile.unit.toString()
             setOnClickListener {
 //                onItemClickListener?.let { it(product_tile) }
             }
