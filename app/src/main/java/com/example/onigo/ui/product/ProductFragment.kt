fe6d33a4b53dd6311ddf6853e66dbd717e35957e
Vitalii Kora <Vitalii_Kora@epam.com>
2024-05-05 12:42:26 +0300

    [Feat]: move setOnClickListener to onCreateViewHolder for ProductTilesAdapter

--- app/src/main/java/com/example/onigo/ui/product/ProductFragment.kt
+++ app/src/main/java/com/example/onigo/ui/product/ProductFragment.kt
@@ -1,7 +1,9 @@
 package com.example.onigo.ui.product
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -10,6 +12,7 @@ import androidx.navigation.fragment.navArgs
 import com.example.onigo.databinding.FragmentProductBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_product.back_button
+import kotlinx.android.synthetic.main.product_tile.view.container
 
 @AndroidEntryPoint
 class ProductFragment : Fragment() {
@@ -32,6 +35,20 @@ class ProductFragment : Fragment() {
 
         back_button.setOnClickListener {
             findNavController().popBackStack()
+        }
+        back_button.setOnTouchListener {item, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    back_button.alpha= 0.6F
+                }
+                MotionEvent.ACTION_CANCEL ->{
+                    back_button.alpha= 1F
+                }
+                MotionEvent.ACTION_UP -> {
+                    item.performClick()
+                }
+            }
+            true
         }
         productIdArg.let { id ->
             mBinding.productId.text = "Product Id: \"$id\""
